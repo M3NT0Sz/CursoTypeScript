6251a6ec79241dd9154bf65e9b48d43fc6535448
Matheus Mendes <103116527+M3NT0Sz@users.noreply.github.com>
2026-01-06 00:10:41 -0300

    Modularizando o TypeScript

--- dist/js/components/nova-transacao-component.js
+++ dist/js/components/nova-transacao-component.js
@@ -1,3 +1,4 @@
+import Conta from "../types/Conta.js";
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -11,23 +12,11 @@ elementoFormulario.addEventListener("submit", function (event) {
     let tipoTransacao = inputTipoTransacao.value;
     let valor = inputValor.valueAsNumber;
     let data = new Date(inputData.value);
-    if (tipoTransacao === TipoTransacao.DEPOSITO) {
-        saldo += valor;
-    }
-    else if (tipoTransacao === TipoTransacao.TRANSFERENCIA ||
-        tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO) {
-        saldo -= valor;
-    }
-    else {
-        alert("Tipo de transação inválido.");
-        return;
-    }
-    elementoSaldo.textContent = formatarMoeda(saldo);
     const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
     };
-    console.log(novaTransacao);
+    Conta.registrarTransacao(novaTransacao);
     elementoFormulario.reset();
 });
---
+++ src/types/Conta.ts
@@ -0,0 +1,31 @@
+import { TipoTransacao } from "./TipoTransacao";
+import { Transacao } from "./Transacao";
+
+let saldo: number = 3000;
+
+const Conta = {
+  getSaldo() {
+    return saldo;
+  },
+
+  getDataAcesso(): Date {
+    return new Date();
+  },
+
+  registrarTransacao(novaTransacao: Transacao): void {
+    if (novaTransacao.tipoTransacao === TipoTransacao.DEPOSITO) {
+      saldo += novaTransacao.valor;
+    } else if (
+      novaTransacao.tipoTransacao === TipoTransacao.TRANSFERENCIA ||
+      novaTransacao.tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO
+    ) {
+      saldo -= novaTransacao.valor;
+    } else {
+      alert("Tipo de transação inválido.");
+      return;
+    }
+    console.log("Transação registrada:", novaTransacao);
+  },
+};
+
+export default Conta;
